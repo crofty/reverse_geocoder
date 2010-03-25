740d2303f9dcb10ca9773a0df9b67e7c411162fb
James Croft <James.R.Croft@googlemail.com>
2010-03-25 20:37:23 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+
+RACK_ENV = 'development'
+
+require 'rubygems'
+require 'sinatra/base'
+require "sequel"
+require 'ruby-debug'
+require 'yajl'
+require 'yajl/json_gem'
+
+# Setup DB
+content = File.new("config/database.yml").read
+settings = YAML::load(content)[RACK_ENV]
+DB = Sequel.connect "#{settings['adapter']}://#{settings['username']}:#{settings['password']}@#{settings['host']}/#{settings['database']}"
+
+Dir.glob(File.join(File.dirname(__FILE__), 'lib/*.rb')).each {|f| require f }
+
+class ReverseGeocoder < Sinatra::Base
+
+  get '/' do
+    JSON.generate Location.new(params[:lng],params[:lng]).address
+  end
+
+end
+
+ReverseGeocoder.run! :host => 'localhost', :port => 9090
+
---
+++ lib/location.rb
@@ -0,0 +1,49 @@
+class Location
+
+  attr_accessor :lat, :lng
+
+  def initialize(lat,lng)
+    @lat = lat
+    @lng = lng
+  end
+
+  def address
+    nearest_roads.first.name
+  end
+
+  def nearest_roads(diff=0.01)
+    xmin = @lng - diff
+    xmax = @lng + diff
+    ymin = @lat - diff
+    ymax = @lat + diff
+    sql = <<-SQL
+      SELECT wt.v AS name, ordered_nodes.distance
+      FROM ways w,
+           way_nodes wn,
+           way_tags wt,
+           (SELECT nearby_nodes.*, ST_Distance(nearby_nodes.geom, GeomFromText('POINT(#{@lng} #{@lat})', 4326)) AS distance
+            FROM 
+              (SELECT n.*
+              FROM nodes n,
+                   way_tags wt,
+                   ways w,
+                   way_nodes wn
+              WHERE (w.bbox && SetSRID('BOX3D(#{xmin} #{ymin}, #{xmax} #{ymax}))'::box3d,4326))
+              AND w.id = wn.way_id
+              AND wn.node_id = n.id
+              AND wt.way_id = w.id
+              AND wt.k = 'highway') nearby_nodes
+            ORDER BY distance ASC
+           )  ordered_nodes
+      WHERE w.id = wn.way_id
+      AND wn.node_id = ordered_nodes.id
+      AND wt.way_id = w.id
+      AND wt.k IN ('name','ref')
+      ORDER BY ordered_nodes.distance ASC, wt.k ASC
+      LIMIT 10
+    SQL
+    DB.run sql
+  end
+end
+
+
